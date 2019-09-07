33bb6eafc7ae523d68488af42c2596c290679ba2
Nori Tan <github@noritan.org>
2019-09-07 23:44:48 +0900

    Update sudoku.py
    Integrated TILE_COLUMNS and TILE_ROWS into TILE_LENGTH
    Adjusted the drawing order of Gray and Black border line

--- sudoku.py
+++ sudoku.py
@@ -4,10 +4,9 @@ TILE_WIDTH = 30
 TILE_HEIGHT = 30
 TILE_GAP = 2
 TILE_GROUP = 3
-TILE_COLMUNS = TILE_GROUP * TILE_GROUP
-TILE_ROWS = TILE_GROUP * TILE_GROUP
-CANVAS_WIDTH = TILE_WIDTH * TILE_COLMUNS + TILE_GAP
-CANVAS_HEIGHT = TILE_HEIGHT * TILE_ROWS + TILE_GAP
+TILE_LENGTH = TILE_GROUP * TILE_GROUP
+CANVAS_WIDTH = TILE_WIDTH * TILE_LENGTH + TILE_GAP
+CANVAS_HEIGHT = TILE_HEIGHT * TILE_LENGTH + TILE_GAP
 WINDOW_WIDTH = 640
 WINDOW_HEIGHT = 480
 CANVAS_X = (WINDOW_WIDTH - CANVAS_WIDTH) / 2
@@ -29,11 +28,27 @@ canvas.create_rectangle(
     fill="gray80",
     width=0
 )
-for i in range(TILE_COLMUNS + 1) :
-    if i % TILE_GROUP == 0:
-        c = "black"
-    else:
-        c = "gray"
+
+# Gray border lines
+c = "gray"
+for i in range(TILE_LENGTH + 1) :
+    x = TILE_WIDTH * i + TILE_GAP * 0.5
+    canvas.create_line(
+        x, 0,
+        x, CANVAS_HEIGHT,
+        fill=c,
+        width=TILE_GAP
+    )
+    y = TILE_HEIGHT * i + TILE_GAP * 0.5
+    canvas.create_line(
+        0, y,
+        CANVAS_WIDTH, y,
+        fill=c,
+        width=TILE_GAP
+    )
+# Black border lines
+c = "black"
+for i in range(0, TILE_LENGTH + 1, TILE_GROUP) :
     x = TILE_WIDTH * i + TILE_GAP * 0.5
     canvas.create_line(
         x, 0,
