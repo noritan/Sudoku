ac78a230cb44f79e234a63f7a12c9e853dfa6c91
Nori Tan <github@noritan.org>
2019-09-07 21:38:38 +0900

    Create sudoku.py
    Initiated skelton window

---
+++ sudoku.py
@@ -0,0 +1,14 @@
+import tkinter
+
+WINDOW_WIDTH = 640
+WINDOW_HEIGHT = 480
+
+# Create a window
+root = tkinter.Tk()
+root.title("Sudoku")
+root.minsize(WINDOW_WIDTH, WINDOW_HEIGHT)
+
+# the main loop
+root.mainloop()
+
+print("Done")
