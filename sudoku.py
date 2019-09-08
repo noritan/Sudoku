3540ce3bb7ef625afe12cfea063d03ab7f554893
Nori Tan <github@noritan.org>
2019-09-08 13:10:24 +0900

    Update sudoku.py
    Added example boards
    Implemented solve button
    Added solver algorithm

--- sudoku.py
+++ sudoku.py
@@ -6,6 +6,7 @@ TILE_FONT = ("monospace", 24)
 TILE_GAP = 2
 TILE_GROUP = 3
 TILE_LENGTH = TILE_GROUP * TILE_GROUP
+TILE_AVAILABLE = frozenset({i+1 for i in range(TILE_LENGTH)})
 CANVAS_WIDTH = TILE_WIDTH * TILE_LENGTH + TILE_GAP
 CANVAS_HEIGHT = TILE_HEIGHT * TILE_LENGTH + TILE_GAP
 WINDOW_WIDTH = 640
@@ -20,10 +21,11 @@ class Square:
         self.col = col
         self.row = row
         self.status = "free"
-        self.number = -1 # unassigned
+        self.number = [] # unassigned
     
     # Assign a number to the Square
     def assign(self, number, status="assigned"):
+        assert(number in TILE_AVAILABLE)
         self.number = number
         self.status = status
 
@@ -68,28 +70,47 @@ def initialBoard():
 
 # Initialize with example board
 def exampleBoard():
+    x = -1
+    Q1 = [
+        x,x,x,3,x,4,x,x,x,
+        x,x,x,x,7,x,9,x,x,
+        x,x,x,x,x,1,x,8,x,
+        1,x,x,x,x,x,4,x,7,
+        x,9,x,x,6,x,x,2,x,
+        4,x,5,x,x,x,x,x,6,
+        x,6,x,2,x,x,x,x,x,
+        x,x,7,x,4,x,x,x,x,
+        x,x,x,8,x,3,x,x,x
+    ]
+    Q2 = [
+        x,x,6,x,x,x,5,x,x,
+        x,x,x,5,x,7,x,x,x,
+        2,x,x,x,x,x,x,x,8,
+        x,4,x,x,6,x,x,7,x,
+        x,x,3,8,x,5,6,x,x,
+        x,9,x,x,3,x,x,8,x,
+        1,x,x,x,x,x,x,x,2,
+        x,x,x,2,x,4,x,x,x,
+        x,x,4,x,x,x,1,x,x
+    ]
+    Q100 = [
+        x,3,x,x,5,x,x,6,x,
+        7,x,x,1,x,x,x,x,8,
+        x,x,2,x,x,x,x,x,x,
+        x,9,x,x,x,8,4,x,x,
+        x,4,x,x,x,x,x,1,x,
+        x,x,6,7,x,x,x,2,x,
+        x,x,x,x,x,x,3,x,x,
+        5,x,x,x,x,4,x,x,6,
+        x,6,x,x,9,x,x,5,x
+    ]
     board = initialBoard()
-    board[0][3].assign(3, "fixed")
-    board[0][5].assign(4, "fixed")
-    board[1][4].assign(7, "fixed")
-    board[1][6].assign(9, "fixed")
-    board[2][5].assign(1, "fixed")
-    board[2][7].assign(8, "fixed")   
-    board[3][0].assign(1, "fixed")
-    board[3][6].assign(4, "fixed")
-    board[3][8].assign(7, "fixed")   
-    board[4][1].assign(9, "fixed")
-    board[4][4].assign(6, "fixed")
-    board[4][7].assign(2, "fixed")
-    board[5][0].assign(4, "fixed")
-    board[5][2].assign(5, "fixed")
-    board[5][8].assign(6, "fixed")
-    board[6][1].assign(6, "fixed")
-    board[6][3].assign(2, "fixed")
-    board[7][2].assign(7, "fixed")
-    board[7][4].assign(4, "fixed")
-    board[8][3].assign(8, "fixed")
-    board[8][5].assign(3, "fixed")
+    q = Q100
+    for i in range(len(q)):
+        if q[i] != x:
+            row = int(i/TILE_LENGTH)
+            col = i % TILE_LENGTH
+            board[row][col].assign(q[i], "fixed")
     return board
 
 # Create a window
@@ -149,6 +170,10 @@ for i in range(0, TILE_LENGTH + 1, TILE_GROUP) :
         tag="board"
     )
 
+# Solve button
+solveButton = tkinter.Button(root, text="SOLVE")
+solveButton.place(x=WINDOW_WIDTH/2, y=CANVAS_Y*2+CANVAS_HEIGHT)
+
 # Callback from canvas
 def canvasOnClick(event):
     x = event.x + TILE_GAP
@@ -166,6 +191,92 @@ def canvasOnClick(event):
     print("Clicked %d, %d" % (col, row))
 
 canvas.bind("<Button-1>", canvasOnClick)
+
+# add negative flag in a group
+def addNegative(square):
+    global groups
+    square.negative = set(TILE_AVAILABLE)
+    for group in groups:
+        if square in group:
+            for member in group:
+                member.negative.add(square.number)
+
+# Solver function
+def solve():
+    # Serial square list
+    allSquare = []
+    for line in board:
+        for square in line:
+            allSquare.append(square)
+    # Construct group structure
+    global groups
+    groups = []
+    # row group
+    for col in range(TILE_LENGTH):
+        group = []
+        for row in range(TILE_LENGTH):
+            group.append(board[row][col])
+        groups.append(list(group))
+    # column group
+    for row in range(TILE_LENGTH):
+        group = []
+        for col in range(TILE_LENGTH):
+            group.append(board[row][col])
+        groups.append(group)
+    # sector group
+    for cbase in range(0, TILE_LENGTH, TILE_GROUP):
+        for rbase in range(0, TILE_LENGTH, TILE_GROUP):
+            group = []
+            for col in range(cbase,cbase+TILE_GROUP):
+                for row in range(rbase,rbase+TILE_GROUP):
+                    group.append(board[row][col])
+            groups.append(group)
+    # Clear all negative set
+    for square in allSquare:
+        square.negative = set()
+    # initialize negative set
+    for square in allSquare:
+        if square.status == "fixed":
+            square.negative = set(TILE_AVAILABLE)
+            addNegative(square)
+        elif square.status == "assigned":
+            square.negative = set(TILE_AVAILABLE)
+            addNegative(square)
+    # 
+    solved = False  # Flag indicating solver completion
+    while not solved:
+        solved = True
+        # assign number to last positive
+        for square in allSquare:
+            if len(square.negative) == TILE_LENGTH - 1:
+                square.assign((set(TILE_AVAILABLE) - square.negative).pop())
+                print("Last positive %d at (%d,%d)" % (square.number, square.col, square.row))
+                addNegative(square)
+                solved = False
+        # scan last positive in a group
+        for number in TILE_AVAILABLE:
+            for group in groups:
+                nNegative = 0
+                for square in group:
+                    if number in square.negative:
+                        nNegative = nNegative + 1
+                if nNegative == TILE_LENGTH - 1:
+                    for square in group:
+                        if not (number in square.negative):
+                            square.assign(number)
+                            print("Last in group %d at (%d,%d)" % (square.number, square.col, square.row))
+                            addNegative(square)
+                            solved = False
+    # Solved or no other solutions
+    print("SOLVED")
+
+# Callback from SOLVE button
+def solveButtonOnClick():
+    solve()
+    drawBoard(board)
+    root.update()
+
+solveButton["command"]=solveButtonOnClick
 
 # Create an example board
 board = exampleBoard()
